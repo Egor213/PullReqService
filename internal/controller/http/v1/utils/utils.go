71be546d33beafcacfd108d2d5ef8dfb232d0bad
Egor213 <zamok7707@gmail.com>
2025-11-14 18:35:02 +0300

    update NewErrReasonJSON

--- internal/controller/http/v1/utils/utils.go
+++ internal/controller/http/v1/utils/utils.go
@@ -3,10 +3,17 @@ package httputils
 import (
 	hd "app/internal/controller/http/v1/dto"
 	he "app/internal/controller/http/v1/errors"
+	"fmt"
 
 	"github.com/labstack/echo/v4"
 )
 
 func NewErrReasonJSON(c echo.Context, httpCode int, msgCode he.ErrorCode, msg string) error {
-	return c.JSON(httpCode, hd.ErrorOutput{Error: hd.APIError{Code: msgCode, Message: msg}})
+	c.JSON(httpCode, hd.ErrorOutput{
+		Error: hd.APIError{
+			Code:    msgCode,
+			Message: msg,
+		},
+	})
+	return fmt.Errorf("%s: %s", msgCode, msg)
 }
