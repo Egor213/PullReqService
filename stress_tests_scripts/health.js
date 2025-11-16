e0853830165df5434c00f4e31f4e078c100f5642
Egor213 <zamok7707@gmail.com>
2025-11-16 14:09:40 +0300

    fix health stress test

--- stress_tests_scripts/health.js
+++ stress_tests_scripts/health.js
@@ -5,23 +5,15 @@ import { Rate } from "k6/metrics";
 export let successRate = new Rate("successful_requests");
 
 export let options = {
-    vus: 300,
+    vus: 5,
     duration: '60s',
 };
 
 
-export default function (data) {
-    const teamName = randomTeam();
-
-    const res = http.get(`http://localhost:8080/health}`, {
-        headers: {
-            Authorization: `Bearer ${data.token}`,
-        },
-    });
-
+export default function () {
+    const res = http.get(`http://localhost:8080/health`);
     const result = check(res, {
         "status is 200": (r) => r.status === 200,
-        "response has correct team_name": (r) => r.json("team_name") === teamName,
     });
 
     successRate.add(result);
